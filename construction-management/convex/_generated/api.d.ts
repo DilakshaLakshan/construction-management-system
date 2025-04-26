3274ff3d3e35781c601713cb034447a11cb7f9ec
Navoda Deepadee <165695174+Navoda-Perera@users.noreply.github.com>
2025-04-26 17:05:10 +0530

    new

--- construction-management/convex/_generated/api.d.ts
+++ construction-management/convex/_generated/api.d.ts
@@ -13,6 +13,9 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as feedback from "../feedback.js";
+import type * as sellTimbet from "../sellTimbet.js";
+import type * as timber from "../timber.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -22,7 +25,11 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  feedback: typeof feedback;
+  sellTimbet: typeof sellTimbet;
+  timber: typeof timber;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
