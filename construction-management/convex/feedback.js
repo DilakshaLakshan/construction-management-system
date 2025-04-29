a1d81ec8c4dba32d9c0006360deb1ff107a9747b
Navoda Deepadee <165695174+Navoda-Perera@users.noreply.github.com>
2025-04-29 18:06:43 +0530

    convex feedback,js

--- construction-management/convex/feedback.js
+++ construction-management/convex/feedback.js
@@ -4,22 +4,32 @@ import { v } from "convex/values";
 // Add feedback
 export const addFeedback = mutation({
   args: {
-    buyerName: v.string(),
-    buyerContact: v.string(),
     timberType: v.string(),
-    quantity: v.number(),
     rating: v.number(),
     comment: v.string(),
   },
   handler: async (ctx, args) => {
-    const feedbackId = await ctx.db.insert("feedback", args);
-    return feedbackId;
-  },
+    const feedbackData = await ctx.db.query("feedback", args)
+    .filter(q=>q.eq(q.field("buyerName"), args.buyerName))
+    .collect();
+
+    if (feedbackData.length==0) 
+    {
+      const data = {
+        timberType: args.timberType,
+        rating: 5,
+        comment: args.comment
+      }
+      const result = await ctx.db.insert("feedback", {
+        ...data,
+      });
+      console.log(result);
+      return result;
+      
+    
+    }
+
+    return feedbackData[0]
+  }
 });
 
-// Get all feedback
-export const getFeedback = query({
-  handler: async (ctx) => {
-    return await ctx.db.query("feedback").collect();
-  },
-});
